db4b345c20ab446e390c38f0b4be1c66a36a83ff
RudychikFedorV <rudychik.fedor@gmail.com>
2024-03-03 17:25:18 +0200

    dom

---
+++ dom.js
@@ -0,0 +1,109 @@
+// Создать div
+const div = document.createElement("div");
+// Добавить к нему класс wrapper
+div.classList.add("wrapper");
+// Поместить его внутрь тэга body
+const body = document.querySelector("body");
+body.appendChild(div);
+// Создать заголовок H1 "DOM (Document Object Model)"
+const header = document.createElement("h1");
+header.textContent = "DOM";
+// Добавить H1 перед DIV с классом wrapper
+div.insertAdjacentElement("beforebegin", header);
+// Создать список <ul></ul>
+// Добавить в него 3 элемента с текстом "1, 2, 3"
+const ul = `
+<ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+</ul>
+`;
+// Поместить список внутрь элемента с классом wrapper
+div.innerHTML = ul;
+console.log(body);
+// =================================================
+// Создать изображение
+const img = document.createElement("img");
+// Добавить следующие свойства к изображению
+// 1. Добавить атрибут source
+img.src = "https://picsum.photos/240";
+// 2. Добавить атрибут width со значением 240
+img.width = 240;
+// 3. Добавить класс hello
+img.classList.add("hello");
+// 4. Добавить свойство alt со значением "Super Man"
+img.alt = "Super Man";
+// Поместить изображение внутрь элемента с классом wrapper
+div.appendChild(img);
+// Используя HTML строку, создать DIV с классом 'pDiv' + c 2-мя параграфами
+const elemHTML = `
+<div class='pDiv'>
+    <p>Параграф 1</p>
+    <p>Параграф 2</p>
+</div>
+`;
+// Поместить этот DIV до элемента <ul></ul>
+const ulList = div.querySelector("ul");
+ulList.insertAdjacentHTML("beforebegin", elemHTML);
+// Добавить для 2-го параграфа класс text
+const pDiv = document.querySelector(".pDiv");
+pDiv.children[1].classList.add("text");
+// Удалить 1-й параграф
+pDiv.firstElementChild.remove();
+
+// Создать функцию generateAutoCard,
+// которая принимает 5 аргументов: brand, color, year
+const generateAutoCard = (brand, color, year) => {
+  const curDate = new Date();
+  const curYear = curDate.getFullYear();
+  return `
+    <div class='autoCard'>
+        <h2>${brand.toUpperCase()} ${year}</h2>
+        <p>Авто ${brand.toUpperCase()} - ${year} года. Возраст автомобиля - ${
+    curYear - year
+  } лет.</p>
+        <button tupe='button' class='btn'>Удалить</button>
+    </div>
+    `;
+};
+// Функция должна возвращать разметку HTML:
+// <div class="autoCard">
+//   <h2>BRAND YEAR</h2>
+//   <p>Автомобиль BRAND - YEAR года. Возраст авто - YEARS лет.</p>
+// </div>
+
+// Создать новый DIV с классом autos
+const carsDiv = document.createElement("div");
+carsDiv.classList.add("autos");
+// Создать 5 карточек авто, используя функцию generateAutoCard
+const carsList = [
+  { brand: "Tesla", year: 2015, color: "Красный" },
+  { brand: "Lexus", year: 2016, color: "Серебристый" },
+  { brand: "Nissan", year: 2012, color: "Черный" },
+  { brand: "Audi", year: 2017, color: "Белый" },
+  { brand: "Sens", year: 2003, color: "Зеленый" },
+];
+const carsHTML = carsList
+  .map((car) => {
+    return generateAutoCard(car.brand, car.color, car.year);
+  })
+  .join("");
+// Поместить эти карточки внутрь DIV с классом autos
+carsDiv.innerHTML = carsHTML;
+// Поместить DIV c классом autos на страницу DOM - до DIV с классом wrapper
+div.insertAdjacentElement("beforebegin", carsDiv);
+// Добавить кнопку Удалить на каждую карточку авто
+
+// При клике на кнопку - удалять карточку из структуры DOM
+// 1. Выбрать все кнопки
+const buttons = document.querySelectorAll(".btn");
+// 2. Создать функцию удаления
+function handleClick(e) {
+  const currentButton = e.currentTarget;
+  currentButton.closest(".autoCard").remove();
+}
+// 3. Использовать цикл - чтобы повесить обработчик события на каждую кнопку
+buttons.forEach((button) => {
+  button.addEventListener("click", handleClick);
+});
